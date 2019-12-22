3f232d24f51b64242f3e7d7624d3ba9c850a168c
rubihouri <rubihouri@gmail.com>
2019-12-22 21:10:16 +0200

    Update get_epg.py

--- get_epg.py
+++ get_epg.py
@@ -3,6 +3,7 @@ import codecs, os, time, shutil
 import yes,hot, walla_tv
 import logging,sys
 import my_dropbox
+import datetime,time
 
 
 file_path = os.path.dirname (os.path.realpath (__file__))
@@ -22,11 +23,11 @@ def close_header(file_out,):
 logger = None
 log_path = os.path.join ('output', 'log.txt')
 
-if os.path.isfile (log_path):
-    os.remove (log_path)
-
 def set_logger():
 
+    if os.path.isfile (log_path):
+      os.remove (log_path)
+  
     global logger
 
     logging.basicConfig(
@@ -39,53 +40,58 @@ def set_logger():
 
     logger = logging.getLogger()   
 
-set_logger()
-
-
 if __name__ == "__main__":
 
     try:
-        tic = time.time()
+        now_time = datetime.datetime.now ()
+        sleep_time = (datetime.timedelta(days=1) + now_time.replace (hour=1, minute=0, second=0) - now_time).seconds
+        print ('Sleep %d before start'%sleep_time)
+        time.sleep (sleep_time)        
+        while True:    
+          set_logger()
+          tic = time.time()
 
-        if not os.path.isdir ('output'):
-          os.makedirs ('output')        
-        
-        filename = os.path.join ('output', 'guide.xml')
-        file_out = codecs.open(filename, 'w', encoding='utf8')  
+          if not os.path.isdir ('output'):
+            os.makedirs ('output')        
 
-        yes_handle = yes.YES(file_out, logger)    
-        hot_handle = hot.HOT(file_out, logger)    
-        walla_handle = walla_tv.WALLA_TV(file_out, logger)    
-        drop_handle = my_dropbox.DropBox ()        
-        
-        # XML Start
-        print_header (file_out)
-                    
-        # Print Channels Area
-        yes_handle.print_channels ()
-        #hot_handle.print_channels ()
-        walla_handle.print_channels ()
-                       
-        # Print Prog area
-        yes_handle.print_progs ()
-        walla_handle.print_progs ()
-                
-        # When ready replace with 
-        #hot_handle.print_progs ()        
-        
-        if 0:
-            hot_data = drop_handle.download_file ('/epg/hot.xml')
-            file_out.write(hot_data)
-                                      
-        # XML Close
-        close_header (file_out)
+          filename = os.path.join ('output', 'guide.xml')
+          file_out = codecs.open(filename, 'w', encoding='utf8')  
 
-        logger.info ('Total create time %d' % (time.time() - tic))
+          yes_handle = yes.YES(file_out, logger)    
+          hot_handle = hot.HOT(file_out, logger)    
+          walla_handle = walla_tv.WALLA_TV(file_out, logger)    
+          drop_handle = my_dropbox.DropBox ()        
 
-        file_out.close()
+          # XML Start
+          print_header (file_out)
 
-        drop_handle.upload_file (filename, '/epg/guide.xml')
-        drop_handle.upload_file (log_path, '/epg/log.txt')  
+          # Print Channels Area
+          yes_handle.print_channels ()
+          #hot_handle.print_channels ()
+          walla_handle.print_channels ()
+
+          # Print Prog area
+          yes_handle.print_progs ()
+          walla_handle.print_progs ()
+
+          # When ready replace with 
+          #hot_handle.print_progs ()        
+
+          if 0:
+              hot_data = drop_handle.download_file ('/epg/hot.xml')
+              file_out.write(hot_data)
+
+          # XML Close
+          close_header (file_out)
+
+          logger.info ('Total create time %d' % (time.time() - tic))
+
+          file_out.close()
+
+          drop_handle.upload_file (filename, '/epg/guide.xml')
+          drop_handle.upload_file (log_path, '/epg/log.txt')  
+          
+          time.sleep (3600)
 
     except:
         logger.exception ('Error during create')
